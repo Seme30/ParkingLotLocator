25e65880f23f4c9a3cf130593e62547ac0a31ef4
Seme30 <semahegn.adugna1@gmail.com>
2024-02-14 15:07:23 +0300

    store token on shared preference

--- app/src/main/java/com/gebeya/parking_lot/data/datapreference/DataStoreRepository.kt
+++ app/src/main/java/com/gebeya/parking_lot/data/datapreference/DataStoreRepository.kt
@@ -6,7 +6,10 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.core.booleanPreferencesKey
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import com.gebeya.parking_lot.data.network.model.PhoneVerifyResponse
+import com.google.gson.Gson
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
@@ -18,9 +21,11 @@ class DataStoreRepository(context: Context) {
 
     private object PreferencesKey {
         val onBoardingKey = booleanPreferencesKey(name = "on_boarding_completed")
+        val authenticationTokenKey = stringPreferencesKey(name = "authentication_token")
     }
 
     private val dataStore = context.dataStore
+    private val gson = Gson()
 
     suspend fun saveOnBoardingState(completed: Boolean) {
         dataStore.edit { preferences ->
@@ -40,6 +45,33 @@ class DataStoreRepository(context: Context) {
             .map { preferences ->
                 val onBoardingState = preferences[PreferencesKey.onBoardingKey] ?: false
                 onBoardingState
+            }
+    }
+
+    suspend fun saveAuthenticationToken(authenticationToken: PhoneVerifyResponse) {
+        val tokenJson = gson.toJson(authenticationToken)
+        println("Login result json: $tokenJson")
+        dataStore.edit { preferences ->
+            preferences[PreferencesKey.authenticationTokenKey] = tokenJson
+        }
+    }
+
+    fun getAuthenticationToken(): Flow<PhoneVerifyResponse?> {
+        return dataStore.data
+            .catch { exception ->
+                if (exception is IOException) {
+                    emit(emptyPreferences())
+                } else {
+                    throw exception
+                }
+            }
+            .map { preferences ->
+                val tokenJson = preferences[PreferencesKey.authenticationTokenKey]
+                if(tokenJson != null){
+                    gson.fromJson(tokenJson, PhoneVerifyResponse::class.java)
+                }else{
+                    null
+                }
             }
     }
 }
